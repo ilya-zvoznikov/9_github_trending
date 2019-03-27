13c8d6e1e8c980bc69edaeb96ab86ad14e24d7ac
Ilya Zvoznikov <ilya.zvoznikov@gmail.com>
2019-03-28 00:26:55 +0700

    first attempt

--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,29 @@
+import requests
 
 
 def get_trending_repositories(top_size):
-    pass
+    headers = {
+        'content-type': 'Accept: application/vnd.github.mercy-preview+json',
+    }
+    params = {
+        'q': 'created:2019-03-26',
+        'sort': 'stars',
+        'order': 'desc',
+        'per_page': '20',
+    }
+    response = requests.get('https://api.github.com/search/repositories',
+                            headers=headers,
+                            params=params
+                            )
+    r = response.json()
+    for item in r['items']:
+        print(item['stargazers_count'])
+        # print(item['created_at'])
 
 
 def get_open_issues_amount(repo_owner, repo_name):
     pass
 
+
 if __name__ == '__main__':
-    pass
+    get_trending_repositories(None)
